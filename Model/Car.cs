4f9bdaba2d0ffd857eecefc456e43fdc2fa31c6d
NLevi <nagyl@kkszki.hu>
2024-09-24 12:54:45 +0200

    Car class is created

---
+++ Model/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto.Model
+{
+    internal class Car
+    {
+        public string Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string License { get; set; }
+        public DateTime Date { get; set; }
+
+
+    }
+}
