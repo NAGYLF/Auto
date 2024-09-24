9ec0f810fb724192b24e0fc6c927e78c834ad6c5
NLevi <nagyl@kkszki.hu>
2024-09-24 14:11:22 +0200

    feladat kész

--- Program.cs
+++ Program.cs
@@ -38,6 +38,49 @@ namespace Auto
             
             conn.Connection.Close();
         }
+        public static void AddCar()
+        {
+            conn.Connection.Open();
+            Car car = new Car();
+            Console.WriteLine("adja meg a Brand");
+            car.Brand = Console.ReadLine();
+            Console.WriteLine("adja meg a Type");
+            car.Type = Console.ReadLine();
+            Console.WriteLine("adja meg a License");
+            car.License = Console.ReadLine();
+            Console.WriteLine("adja meg a Date");
+            car.Date = int.Parse(Console.ReadLine());
+
+            string sql = $"INSERT INTO `cars`(`Brand`, `Type`, `License`, `Date`) VALUES ('{car.Brand}','{car.Type}','{car.License}',{car.Date})";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+        }
+
+        public static void UpdateCar() 
+        {
+            conn.Connection.Open();
+            int date = 2550;
+            string sql = $"UPDATE `cars` SET `Date`={date} WHERE Id = 223";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+        }
+        public static void DeleteCar()
+        {
+            conn.Connection.Open();
+            int id = 305;
+            string sql = $"DELETE FROM `cars` WHERE `Id`={id}";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+        }
         static void Main(string[] args)
         {
             feltoltes();
@@ -45,6 +88,9 @@ namespace Auto
             {
                 Console.WriteLine($"Auto gyártója: {item.Brand}, azonosítója: {item.Id}");
             }
+            AddCar();
+            UpdateCar();
+            DeleteCar();
             Console.ReadLine();
         }
     }
